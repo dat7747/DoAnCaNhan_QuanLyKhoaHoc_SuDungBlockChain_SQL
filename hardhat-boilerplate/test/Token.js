3e510b151659f86045e636ed3c30c0c373115ab7
dat7747 <132331320+dat7747@users.noreply.github.com>
2024-07-07 20:33:22 +0700

    Test Smart MarketPlace

--- hardhat-boilerplate/test/Token.js
+++ hardhat-boilerplate/test/Token.js
@@ -1,69 +1,91 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("CourseRegistration", function () {
-    let owner, student, token, vault, courseRegistration;
+describe("HeroMarketplace", function () {
+  let HeroMarketplace, heroMarketplace, owner, addr1, addr2;
+  let NFT, nft;
+  let Token, token;
 
-    beforeEach(async function () {
-        [owner, student] = await ethers.getSigners();
+  beforeEach(async function () {
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
 
-        // Triển khai Token
-        const Token = await ethers.getContractFactory("Token");
-        token = await Token.deploy();
-        await token.deployed();
+    // Triển khai hợp đồng NFT (giả lập)
+    NFT = await ethers.getContractFactory("Hero");
+    nft = await NFT.deploy();
+    await nft.deployed();
 
-        // Triển khai Vault
-        const Vault = await ethers.getContractFactory("Vault");
-        vault = await Vault.deploy(token.address);
-        await vault.deployed();
+    // Triển khai hợp đồng Token ERC20 (giả lập)
+    Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy();
+    await token.deployed();
 
-        // Triển khai CourseRegistration
-        const CourseRegistration = await ethers.getContractFactory("CourseRegistration");
-        courseRegistration = await CourseRegistration.deploy(token.address, vault.address);
-        await courseRegistration.deployed();
+    // Triển khai hợp đồng HeroMarketplace
+    HeroMarketplace = await ethers.getContractFactory("HeroMarketplace");
+    heroMarketplace = await HeroMarketplace.deploy(token.address, nft.address);
+    await heroMarketplace.deployed();
+  });
 
-        // Chủ sở hữu thêm khóa học mới
-        const courseId = 1;
-        const price = ethers.utils.parseEther("1"); // 1 token
-        const sessions = 10;
-        await courseRegistration.connect(owner).addCourse(courseId, price, sessions);
+  it("should set the correct owner", async function () {
+    expect(await heroMarketplace.owner()).to.equal(owner.address);
+  });
 
-        // Chuyển đủ số token vào tài khoản học viên
-        const amountToTransfer = ethers.utils.parseEther("10"); // 10 tokens
-        await token.transfer(student.address, amountToTransfer);
+  it("should list an NFT", async function () {
+    // Mint một NFT cho addr1
+    await nft.connect(owner).mint(addr1.address, 1);
+    expect(await nft.ownerOf(1)).to.equal(addr1.address);
 
-        // Học viên phê duyệt token cho CourseRegistration
-        await token.connect(student).approve(courseRegistration.address, amountToTransfer);
-    });
+    // Approve marketplace để chuyển NFT
+    await nft.connect(addr1).approve(heroMarketplace.address, 1);
 
-    it("should allow student to register for a course", async function () {
-        const courseId = 1;
+    // List NFT
+    await heroMarketplace.connect(addr1).listNft(1, ethers.utils.parseUnits("1", "ether"));
 
-        // Kiểm tra số dư của học viên trước khi đăng ký
-        const initialStudentBalance = await token.balanceOf(student.address);
-        console.log("Initial student balance:", ethers.utils.formatEther(initialStudentBalance));
+    // Kiểm tra xem NFT đã được list chưa
+    const listedNfts = await heroMarketplace.getListedNft();
+    expect(listedNfts.length).to.equal(1);
+    expect(listedNfts[0].author).to.equal(addr1.address);
+    expect(listedNfts[0].price).to.equal(ethers.utils.parseUnits("1", "ether"));
+    expect(listedNfts[0].tokenId).to.equal(1);
+  });
 
-        // Học viên đăng ký khóa học
-        await courseRegistration.connect(student).register(courseId);
+  it("should allow buying a listed NFT", async function () {
+    // Mint một NFT cho addr1
+    await nft.connect(owner).mint(addr1.address, 1);
+    expect(await nft.ownerOf(1)).to.equal(addr1.address);
 
-        // Kiểm tra số dư của học viên sau khi đăng ký
-        const finalStudentBalance = await token.balanceOf(student.address);
-        console.log("Final student balance:", ethers.utils.formatEther(finalStudentBalance));
+    // Approve marketplace để chuyển NFT
+    await nft.connect(addr1).approve(heroMarketplace.address, 1);
 
-        // Kiểm tra số dư của vault
-        const vaultBalance = await token.balanceOf(vault.address);
-        console.log("Vault balance after registration:", ethers.utils.formatEther(vaultBalance));
+    // List NFT
+    await heroMarketplace.connect(addr1).listNft(1, ethers.utils.parseUnits("1", "ether"));
 
-        // Kiểm tra xem học viên đã được đăng ký chưa
-        const isRegistered = await courseRegistration.registrationStatus(courseId, student.address);
-        expect(isRegistered).to.be.true;
+    // Mint và approve token cho addr2 để mua NFT
+    await token.connect(owner).transfer(addr2.address, ethers.utils.parseUnits("10", "ether"));
+    await token.connect(addr2).approve(heroMarketplace.address, ethers.utils.parseUnits("1", "ether"));
 
-        // Kiểm tra sự kiện Registration
-        await expect(courseRegistration.connect(student).register(courseId))
-            .to.emit(courseRegistration, "Registration")
-            .withArgs(student.address, courseId);
+    // Mua NFT
+    await heroMarketplace.connect(addr2).buyNft(1, ethers.utils.parseUnits("1", "ether"));
 
-        // Kiểm tra số dư token của học viên sau khi đăng ký
-        expect(finalStudentBalance).to.equal(initialStudentBalance.sub(ethers.utils.parseEther("1")));
-    });
+    // Kiểm tra xem NFT đã được chuyển sang addr2 chưa
+    expect(await nft.ownerOf(1)).to.equal(addr2.address);
+  });
+
+  it("should unlist an NFT", async function () {
+    // Mint một NFT cho addr1
+    await nft.connect(owner).mint(addr1.address, 1);
+    expect(await nft.ownerOf(1)).to.equal(addr1.address);
+
+    // Approve marketplace để chuyển NFT
+    await nft.connect(addr1).approve(heroMarketplace.address, 1);
+
+    // List NFT
+    await heroMarketplace.connect(addr1).listNft(1, ethers.utils.parseUnits("1", "ether"));
+
+    // Unlist NFT
+    await heroMarketplace.connect(addr1).unlistNft(1);
+
+    // Kiểm tra xem NFT đã được unlist chưa
+    const listedNfts = await heroMarketplace.getListedNft();
+    expect(listedNfts.length).to.equal(0);
+  });
 });
